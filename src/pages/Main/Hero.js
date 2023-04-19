5d24f9f0d21e023b66003a55644546219916caa5
beerobinson <bee.robinson91@gmail.com>
2023-04-19 16:36:44 -0400

    inserting header image

--- src/pages/Main/Hero.js
+++ src/pages/Main/Hero.js
@@ -1,1 +1,9 @@
-function header()
+import React from "react";
+
+import interior from '../../images/cafe interior.jpg'
+
+export default function Hero(){
+    return(
+        <img src={interior} className="interior-pic" alt="Henry's Cafe Interior" />
+    )
+}
